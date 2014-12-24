6934809448831df351c1eb470d758c2c3cd1b562
Peter Sbarski <peter.sbarski@gmail.com>
2014-12-24 18:22:59 +1100

    changing nuspec file

--- BuildNugetPackage.bat
+++ BuildNugetPackage.bat
@@ -1,4 +1,4 @@
 %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild src/kinesisnet/kinesisnet.csproj /target:Clean
 %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild src/kinesisnet/kinesisnet.csproj /P:Configuration=Release
 
-nuget\nuget.exe pack src\kinesisnet\kinesisnet.csproj -Prop Configuration=Release -OutputDirectory nuget
+nuget\nuget.exe pack src\kinesisnet\KinesisNet.csproj -Prop Configuration=Release -OutputDirectory nuget
